827515c55e3edb29501a595e0f41e01fbda5725d
gvillalonga89 <gvillalonga@cvc.uab.es>
2018-02-02 19:40:16 +0100

    Optimizer builder does not check major capital characters anymore.

--- utils/optimizer_builder.py
+++ utils/optimizer_builder.py
@@ -6,21 +6,21 @@ class Optimizer_builder():
         pass
         
     def build(self, cf, net):
-        if cf.optimizer == 'Adam':
+        if cf.optimizer.lower() == 'adam':
             return optim.Adam([
                 {'params': [param for name, param in net.named_parameters() if name[-4:] == 'bias'],
                      'lr': float(cf.learning_rate_bias)},
                 {'params': [param for name, param in net.named_parameters() if name[-4:] != 'bias'],
                      'lr': float(cf.learning_rate), 'weight_decay': float(cf.weight_decay)}], 
                 betas=(cf.momentum1, cf.momentum2), eps=1e-08)
-        elif cf.optimizer == 'RMSprop':
+        elif cf.optimizer.lower() == 'rmsprop':
             return optim.RMSprop([
                 {'params': [param for name, param in net.named_parameters() if name[-4:] == 'bias'],
                      'lr': cf.learning_rate_bias},
                 {'params': [param for name, param in net.named_parameters() if name[-4:] != 'bias'],
                      'lr': cf.learning_rate, 'weight_decay': cf.weight_decay}], 
                 alpha=0.99, eps=1e-08, momentum=cf.momentum1, centered=False)
-        elif cf.optimizer == 'SGD':
+        elif cf.optimizer.lower() == 'sgd':
             return optim.SGD([
                 {'params': [param for name, param in net.named_parameters() if name[-4:] == 'bias'],
                      'lr': cf.learning_rate_bias},
